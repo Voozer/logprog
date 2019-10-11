0dfda588334176d8d2cd9ecc7634c5a7ee96107f
Voozer <55439253+Voozer@users.noreply.github.com>
2019-10-11 22:36:33 +0300

    Add files via upload

---
+++ fam.pl
@@ -0,0 +1,106 @@
+male('Sergei Ilminsky').
+male('Nikita Ilminsky').
+male('Vasiliy Zakharov').
+male('Alexander Zakharov').
+male('Gleb Ilminsky').
+male('Maksim Kulaga').
+male('Artem Mkrtchyan').
+male('Vyacheslav Mkrtchyan').
+male('Vladimir Samotokhin').
+male('Vasiliy Zakharov').
+male('Mikhail Zakharov').
+male('Alexander Ilminsky').
+male('Ivan Ilminsky').
+male('Kuzma Levchenko').
+male('Grigoriy Mkrtchyan').
+male('Fedor Zakharov').
+male('Stepan Zakharov').
+male('Boris Zakharov').
+male('Alexey Zakharov').
+male('Andrey Samotokhin').
+male('Alexander Samotokhin').
+male('Pavel Zakharov').
+male('Daniil Samotokhin').
+male('Mikhail Samotokhin').
+
+female('Olga Zakharova').
+female('Valentina Zakharova').
+female('Ludmila Zakharova').
+female('Anastasiya Ilminskaya').
+female('Evdokiya Levchenko').
+female('Nataliya Ilminskaya').
+female('Anna Kulaga').
+female('Rimma Pakhomcheva').
+female('Anastasiya N').
+female('Ogniya N').
+female('Alyona Kulaga').
+female('Juliya Kulaga').
+female('Lyubov Zakharova').
+female('Axinha N').
+female('Tatiyana Zakharova').
+female('Marusya Zakharova').
+female('Olga Samotokhina').
+female('Tamara Zakharova').
+female('Polina Zakharova').
+female('Ekaterina Samotokhina').
+female('Mariya Samotokhina').
+female('Kira Samotokhina').
+
+child('Nikita Ilminsky', 'Sergei Ilminsky').
+child('Nikita Ilminsky', 'Olga Zakharova').
+child('Valentina Zakharova', 'Vasiliy Zakharov').
+child('Valentina Zakharova', 'Lyubov Zakharova').
+child('Ludmila Zakharova', 'Vasiliy Zakharov').
+child('Ludmila Zakharova', 'Lyubov Zakharova').
+child('Olga Zakharova', 'Vasiliy Zakharov').
+child('Olga Zakharova', 'Lyubov Zakharova').
+child('Alexander Zakharov', 'Vasiliy Zakharov').
+child('Alexander Zakharov', 'Lyubov Zakharova').
+child('Gleb Ilminsky', 'Sergei Ilminsky').
+child('Anastasiya Ilminskaya', 'Sergei Ilminsky').
+child('Sergei Ilminsky', 'Alexander Ilminsky').
+child('Sergei Ilminsky', 'Evdokiya Levchenko').
+child('Nataliya Ilminskaya', 'Alexander Ilminsky').
+child('Nataliya Ilminskaya', 'Evdokiya Levchenko').
+child('Anna Kulaga', 'Maksim Kulaga').
+child('Anna Kulaga', 'Nataliya Ilminskaya').
+child('Alyona Kulaga', 'Maksim Kulaga').
+child('Alyona Kulaga', 'Nataliya Ilminskaya').
+child('Juliya Kulaga', 'Maksim Kulaga').
+child('Juliya Kulaga', 'Nataliya Ilminskaya').
+child('Artem Mkrtchyan', 'Vyacheslav Mkrtchyan').
+child('Artem Mkrtchyan', 'Ludmila Zakharova').
+child('Alexander Samotokhin', 'Vladimir Samotokhin').
+child('Alexander Samotokhin', 'Valentina Zakharova').
+child('Olga Samotokhina', 'Vladimir Samotokhin').
+child('Olga Samotokhina', 'Valentina Zakharova').
+child('Andrey Samotokhin', 'Vladimir Samotokhin').
+child('Andrey Samotokhin', 'Valentina Zakharova').
+child('Vasiliy Zakharov', 'Alexander Zakharov').
+child('Vasiliy Zakharov', 'Rimma Pakhomcheva').
+child('Mikhail Zakharov', 'Alexander Zakharov').
+child('Mikhail Zakharov', 'Rimma Pakhomcheva').
+child('Alexander Ilminsky', 'Ivan Ilminsky').
+child('Alexander Ilminsky', 'Anastasiya N').
+child('Evdokiya Levchenko', 'Kuzma Levchenko').
+child('Evdokiya Levchenko', 'Ogniya N').
+child('Grigoriy Mkrtchyan', 'Artem Mkrtchyan').
+child('Vasiliy Zakharov', 'Fedor Zakharov').
+child('Vasiliy Zakharov', 'Axinha N').
+child('Tamara Zakharova', 'Fedor Zakharov').
+child('Tamara Zakharova', 'Axinha N').
+child('Boris Zakharov', 'Stepan Zakharov').
+child('Boris Zakharov', 'Tatiyana Zakharova').
+child('Alexey Zakharov', 'Stepan Zakharov').
+child('Alexey Zakharov', 'Tatiyana Zakharova').
+child('Marusya Zakharova', 'Stepan Zakharov').
+child('Marusya Zakharova', 'Tatiyana Zakharova').
+child('Lyubov Zakharova', 'Stepan Zakharov').
+child('Lyubov Zakharova', 'Tatiyana Zakharova').
+child('Polina Zakharova', 'Vasiliy Zakharov').
+child('Pavel Zakharov', 'Mikhail Zakharov').
+child('Kira Samotokhina', 'Andrey Samotokhin').
+child('Mikhail Samotokhin', 'Andrey Samotokhin').
+child('Ekaterina Samotokhina', 'Alexander Samotokhin').
+child('Mariya Samotokhina', 'Alexander Samotokhin').
+child('Daniil Samotokhin', 'Olga Samotokhina').
---
+++ parser.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+auto npos = std::string::npos;
+
+struct person {
+    string ID;
+    string firstName;
+    string surName;
+    string sex;
+};
+
+struct family {
+    string dadID = "none";
+    string momID = "none";
+    vector<string> childrenID;
+};
+
+person findPerson(string ID, vector<person> v) {
+    for (size_t i = 0; i < v.size(); i++) {
+        if (v[i].ID == ID) {
+            return v[i];
+        }
+    }
+}
+
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3) {
+        cout << "Usage: ./a.out inputFile outputFile";
+        exit(1);
+    }
+    ifstream input;
+    input.open(argv[1]);
+    ofstream output;
+    output.open(argv[2]);
+
+    string str;
+    person p;
+    family f;
+    vector<person> people;
+    vector<family> families;
+    size_t predicateCounter = 0;
+
+    while (getline(input, str)) {
+        if (str.find("INDI") != npos) {
+            size_t i = 2;
+            while (str[i + 1] != '@') {
+                i++;
+            }
+            p.ID = str.substr(3, i - 2);
+            while (predicateCounter < 3) {
+                if (str.find("GIVN") != npos) {
+                    p.firstName = str.substr(7, npos);
+                    predicateCounter++;
+                }
+                if (str.find("SURN") != npos) {
+                    p.surName = str.substr(7, npos);
+                    predicateCounter++;
+                }
+                if (str.find("SEX") != npos) {
+                    if (str[6] == 'M') {
+                        p.sex = "male";
+                    } else {
+                        p.sex = "female";
+                    }
+                    predicateCounter++;
+                }
+                getline(input, str);
+            }
+            predicateCounter = 0;
+            people.push_back(p);
+        }
+        if (str[0] == '0' && str.find("FAM") != npos) {
+            while (str.find("RIN") == npos) {
+                if (str.find("HUSB") != npos) {
+                    size_t i = 7;
+                    while (str[i + 1] != '@') {
+                        i++;
+                    }
+                    f.dadID = str.substr(8, i - 7);
+                }
+                if (str.find("WIFE") != npos) {
+                    size_t i = 7;
+                    while (str[i + 1] != '@') {
+                        i++;
+                    }
+                    f.momID = str.substr(8, i - 7);
+                }
+                if (str.find("CHIL") != npos) {
+                    size_t i = 7;
+                    while (str[i + 1] != '@') {
+                        i++;
+                    }
+                    f.childrenID.push_back(str.substr(8, i - 7));
+                }
+                getline(input, str);
+            }
+            families.push_back(f);
+            f.dadID = "none";
+            f.momID = "none";
+            f.childrenID.clear();
+        }
+    }
+    for (size_t i = 0; i < people.size(); i++) {
+        if (people[i].sex == "male") {
+            output << people[i].sex << "('" << people[i].firstName << " " << people[i].surName << "')." << endl;
+        }
+    }
+    output << endl;
+    for (size_t i = 0; i < people.size(); i++) {
+        if (people[i].sex == "female") {
+            output << people[i].sex << "('" << people[i].firstName << " " << people[i].surName << "')." << endl;
+        }
+    }
+    output << endl;
+    for (size_t i = 0; i < families.size(); i++) {
+        person dad = findPerson(families[i].dadID, people);
+        person mom = findPerson(families[i].momID, people);
+        for (size_t j = 0; j < families[i].childrenID.size(); j++) {
+            p = findPerson(families[i].childrenID[j], people);
+            if (families[i].dadID != "none") {
+                output << "child('" << p.firstName << " " << p.surName << "', '" << dad.firstName << " " << dad.surName << "')." << endl;
+            }
+            if (families[i].momID != "none") {
+                output << "child('" << p.firstName << " " << p.surName << "', '" << mom.firstName << " " << mom.surName << "')." << endl;
+            }
+        }
+    }    
+    input.close();
+    output.close();
+    return 0;
+}
+
+//с использованием предиката
+//child(ребенок, родитель),
+//male(человек),
+//female(человек)
+//INDI, GIVN, SURN, SEX, FAMC, FAMS, FAM, HUSB, WIFE, CHIL
